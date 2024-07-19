cb5022e285f49de8be09a6f6ea2b14f02a038578
isk2493 <165746542+isk2493@users.noreply.github.com>
2024-07-19 16:14:11 +0900

    Update

--- Assets/Custom.cs
+++ Assets/Custom.cs
@@ -1,158 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.PlayerSettings;
 
 public class Custom : MonoBehaviour
 {
-    enum KEYGET {UP,DOWN,RIGHT,LEFT }
+    enum KEYGET { UP, DOWN, RIGHT, LEFT }
     public Text FixedPartName;
     public Text styleName;
     CharacterCustom player;
     Item item;
-    string StyleCheck="Position";
-    int num=0;
-    public float min=1;
-    public float max=3;
+    string StyleCheck = "Position";
+    int num = 0;
+    public float min = 1;
+    public float max = 3;
     public Slider sliderX;
     public Slider sliderY;
     public Slider sliderZ;
-    public GameObject pos;
-    public GameObject scale;
-    private void Start()
-    {
-        pos.SetActive(true);
-        scale.SetActive(false);
-        sliderX.minValue = min;
-        sliderX.maxValue = max;
-        sliderY.minValue = min;
-        sliderY.maxValue = max;
-        sliderZ.minValue = min;
-        sliderZ.maxValue = max;
-    }
-    public void PlayerSet(Item _item)
-    {        
-        item = _item;
-        player.CharSet();
-    }
+    Vector3 slideSet;
+   
     private void Awake()
     {
         player = FindObjectOfType<CharacterCustom>();
+
     }
     public void Choice(string str)
     {
         StyleCheck = str;
         styleName.text = str;
-        if (str == "Scale")
-        {
-            pos.SetActive(false);
-            scale.SetActive(true);
-        }
-        else
-        {
-            pos.SetActive(true);
-            scale.SetActive(false);
-        }
+        PartSelect(num);
     }
-    public void ScaleSet()
+
+    void ValueSet()
     {
-        player.bodyParts[num].transform.localScale =new Vector3(sliderX.value, sliderY.value, sliderZ.value) ;
+        sliderX.minValue = min; sliderX.maxValue = max;
+        sliderY.minValue = min; sliderY.maxValue = max;
+        sliderZ.minValue = min; sliderZ.maxValue = max;
     }
-    public void StyleLeft(string str)
+    public void SizeCustom()
     {
+        slideSet = new Vector3(sliderX.value, sliderY.value, sliderZ.value);
+
         if (StyleCheck == "Position")
         {
-            if (str == KEYGET.UP.ToString())
+
+            if (num == 2 || num == 1)
             {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Translate(Vector2.up * 0.01f, Space.Self);
-                    player.bodyParts[2].transform.Translate(Vector2.up * 0.01f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Translate(Vector2.up * 0.01f, Space.Self);
+                player.bodyParts[1].transform.localPosition = slideSet;
+                player.bodyParts[2].transform.localPosition = slideSet;
             }
-            else if (str == KEYGET.DOWN.ToString())
-            {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Translate(Vector2.down * 0.01f, Space.Self);
-                    player.bodyParts[2].transform.Translate(Vector2.down * 0.01f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Translate(Vector2.down * 0.01f, Space.Self);
-            }
-            else if (str == KEYGET.LEFT.ToString())
-            {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Translate(Vector2.left * 0.01f, Space.Self);
-                    player.bodyParts[2].transform.Translate(Vector2.left * -0.01f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Translate(Vector2.left * 0.01f, Space.Self);
-            }
-            else if (str == KEYGET.RIGHT.ToString())
-            {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Translate(Vector2.right * 0.01f, Space.Self);
-                    player.bodyParts[2].transform.Translate(Vector2.right * -0.01f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Translate(Vector2.right * 0.01f, Space.Self);
-            }
+            else
+                player.bodyParts[num].transform.localPosition = slideSet;
         }
         else if (StyleCheck == "Rotation")
         {
-            if (str == KEYGET.UP.ToString())
+            if (num == 2 || num == 1)
             {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Rotate(Vector2.up * 1f, Space.Self);
-                    player.bodyParts[2].transform.Rotate(Vector2.up * 1f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Rotate(Vector2.up * 1f, Space.Self);
+                player.bodyParts[1].transform.localRotation=Quaternion.Euler(slideSet);
+                player.bodyParts[2].transform.localRotation = Quaternion.Euler(slideSet);
             }
-            else if (str == KEYGET.DOWN.ToString())
+            else
+                player.bodyParts[num].transform.localRotation = Quaternion.Euler(slideSet);
+        }
+        else if (StyleCheck == "Scale")
+        {
+            if (num == 2 || num == 1)
             {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Rotate(Vector2.down * 1f, Space.Self);
-                    player.bodyParts[2].transform.Rotate(Vector2.down * 1f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Rotate(Vector2.down * 1f, Space.Self);
+                player.bodyParts[1].transform.localScale = slideSet;
+                player.bodyParts[2].transform.localScale = slideSet;
             }
-            else if (str == KEYGET.LEFT.ToString())
-            {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Rotate(Vector2.left * 1f, Space.Self);
-                    player.bodyParts[2].transform.Rotate(Vector2.left * -1f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Rotate(Vector2.left * 1f, Space.Self);
-            }
-            else if (str == KEYGET.RIGHT.ToString())
-            {
-                if (num == 2 || num == 1)
-                {
-                    player.bodyParts[1].transform.Rotate(Vector2.right * 1f, Space.Self);
-                    player.bodyParts[2].transform.Rotate(Vector2.right * -1f, Space.Self);
-                }
-                else
-                    player.bodyParts[num].transform.Rotate(Vector2.right * 1f, Space.Self);
-            }
-        }         
-    } 
+            else
+                player.bodyParts[num].transform.localScale = slideSet;
+        }
+    }
+    //public void StyleSet(string str)
+    //{
+    //    StyleCheck = str;  
+    //}
     public void PartSelect(int _num)
     {
         num = _num;
         FixedPartName.text = player.bodyParts[num].name;
-        sliderX.value = player.bodyParts[num].transform.localScale.x;
-        sliderY.value = player.bodyParts[num].transform.localScale.y;
-        sliderZ.value = player.bodyParts[num].transform.localScale.z;
+        if (StyleCheck == "Position")
+        {
+            min = -0.5f; max = 0.5f;
+            ValueSet();
+            Vector3 pos= player.bodyParts[num].transform.localPosition;
+            sliderX.value = pos.x;
+            sliderY.value = pos.y;
+            sliderZ.value = pos.z;
+            slideSet = new Vector3(sliderX.value, sliderY.value, sliderZ.value);
+        }
+        else if (StyleCheck == "Rotation")
+        {
+            min = -180f; max = 180;
+            ValueSet();
+            Quaternion qu = player.bodyParts[num].transform.localRotation;
+            sliderX.value = qu.x;
+            sliderY.value = qu.y;
+            sliderZ.value = qu.z;
+            slideSet = new Vector3(sliderX.value, sliderY.value, sliderZ.value);
+        }
+        else if (StyleCheck == "Scale")
+        {
+            min = 1; max = 4;
+            ValueSet();
+            
+            Vector3 scale = player.bodyParts[num].transform.localScale;
+            sliderX.value = scale.x;
+            sliderY.value = scale.y;
+            sliderZ.value = scale.z;
+            slideSet= new Vector3(sliderX.value, sliderY.value, sliderZ.value);
+        }
+
     }
 }
