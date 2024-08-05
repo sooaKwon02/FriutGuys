a7cbb8c4bc04ef6d6a2c69acf4ff1c3cdc7d199d
isk2493 <165746542+isk2493@users.noreply.github.com>
2024-08-05 17:27:47 +0900

    123
    123

--- Assets/UserInfo.cs
+++ Assets/UserInfo.cs
@@ -9,98 +9,45 @@ public class UserInfo : MonoBehaviourPunCallbacks
 {
     //[HideInInspector]
     public Text userName;
-    public GameObject readyGame;
+    public Image readyGame;
     public GameObject kickGame;
 
     [HideInInspector]
     public bool isReady;
-
-    public string nickname;
     //private SaveLoad saveLoadScript;
     private Transform userPanel;
 
     private void Awake()
     {
+        isReady = false;
+        readyGame.enabled = false;
         userPanel = GameObject.FindGameObjectWithTag("UserInfoPanel").transform;
         transform.SetParent(userPanel.transform);
+        
     }
-    public void DisplayPlayerInfo()
+    public void DisplayPlayerInfo(string _nick)
     {
-        userName.text = nickname;
-        readyGame.SetActive(isReady);
-
+        userName.text = _nick;
         if (PhotonNetwork.IsMasterClient)
         {
-            //userName.color = Color.blue;
-           
-                kickGame.SetActive(true);            
-           
-                //kickGame.SetActive(false);
-            
+            kickGame.SetActive(true);
         }
         else
         {
             kickGame.SetActive(false);
         }
-
-    }
-
-   
-   
-
-    //private void Awake()
-    //{
-    //    readyCheck = false;
-    //    if (PhotonNetwork.IsMasterClient)
-    //    {
-    //        if(!photonView.IsMine)
-    //        {
-    //            kickGame.SetActive(true);
-    //        }
-    //        else
-    //        {
-    //            kickGame.SetActive(false);
-    //        }
-    //    }              
-    //    else
-    //    {
-    //        kickGame.SetActive(false);
-    //    }
-    //    if (GameObject.FindGameObjectWithTag("UserInfoPanel"))
-    //    {
-    //        transform.SetParent(GameObject.FindGameObjectWithTag("UserInfoPanel").transform);   
-    //    }
-
-    //}
-    //public void KickOutButton()
-    //{
-    //    if (PhotonNetwork.IsMasterClient)
-    //    {
-    //        PhotonView pv = gameObject.GetComponent<PhotonView>();
-    //        photonView.RPC("TriggerPlayerKick",pv.Owner);
-    //    }
-    //}
-    //[PunRPC]
-    //void TriggerPlayerKick()
-    //{
-    //    PhotonNetwork.LeaveRoom();
-    //}
-    //public void Ready(bool check)
-    //{
-    //    if (check)
-    //    {
-    //        readyGame.color = new Color(0, 1, 0, 1);
-    //        readyCheck = true;
-    //    }
-    //    else
-    //    {
-    //        readyGame.color = new Color(1, 1, 1, 0.5f);
-    //        readyCheck = false;
-    //    }
-    //}
-    //public void SetUserInfo(string _name)
-    //{
-    //    userName.text = _name;
-    //    PhotonNetwork.NickName = _name;
-    //}
+    } 
+    public void Ready()
+    {
+        if (!isReady)
+        {
+            isReady = true;
+            readyGame.enabled=true;
+        }
+        else
+        {
+            isReady = false;
+            readyGame.enabled=false;
+        }
+    }   
 }
