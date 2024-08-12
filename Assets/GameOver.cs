93eb8216f583fb5fbe245e84dabe181afacb50b4
isk2493 <165746542+isk2493@users.noreply.github.com>
2024-08-12 16:01:27 +0900

    123
    123

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class GameOver : MonoBehaviour
 {
     public GameObject over;
+    public GameObject obj;  
     public void Win()
     {
         over.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0, 1);
@@ -14,19 +16,19 @@ public class GameOver : MonoBehaviour
         StartCoroutine(OverGame());
     }
     IEnumerator OverGame()
-    {
-        
+    {        
         over.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 1);
-        for(int i=0;i<20;i++)
+        while(over.transform.localPosition.z>-0.2f)
         {
-            yield return new WaitForSeconds(0.1f);
-            over.transform.position+=new Vector3(0,0,0.1f);
+            over.transform.Translate(Vector3.back*0.02f,Space.World);
+            yield return null;  
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.CompareTag("Player"))
         {
+            obj = collision.gameObject;
             collision.transform.GetComponent<PlayerCtrl>().GameOver();
         }
     }
